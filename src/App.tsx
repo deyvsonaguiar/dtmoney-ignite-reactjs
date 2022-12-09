b0e27e5ed68a47154f2b0850643c8bd39eeb18f6
Deyvson Aguiar <deyvsonaguiar@gmail.com>
2022-12-08 23:33:25 -0400

    Creating global styles with styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import styled from 'styled-components'
+import { GlobalStyle } from './styles/global'
 
 export function App() {
   return (
     <div className="App">
       <h1>Hello DTMoney - Ignite Application</h1>
+      <GlobalStyle />
     </div>
   );
 }
